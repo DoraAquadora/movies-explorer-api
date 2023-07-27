96d6707f16f64633827ddf428e3664903337ff2c
Dora Kutsenko <kk.tasha@yandex.ru>
2023-07-28 00:54:38 +0300

    vm

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors({
-
+  origin: ['https://api.doramovies.nomoredomains.sbs', 'http://api.doramovies.nomoredomains.sbs'],
+  credentials: true,
 }));
 
 app.use(requestLogger);
